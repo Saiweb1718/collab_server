94e1ee33ebb9247e885bed79ba8e59947d770e18
KarruHarin <harinkarru@gmail.com>
2026-01-19 23:26:03 +0530

    luv u saideep

--- src/chat/socket.js
+++ src/chat/socket.js
@@ -1,1 +1,30 @@
-//socket initialization
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*", 
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("message", (data) => {
+    console.log(data);
+    io.emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
